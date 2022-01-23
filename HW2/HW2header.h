576eafd45ef1e0b7470205782061a315776074a7
blue_lobster <42709757+vincent-tvincent@users.noreply.github.com>
2022-01-23 02:05:02 -0500

    a bit more after a bit more

--- HW2/HW2header.h
+++ HW2/HW2header.h
@@ -1,10 +1,12 @@
 /*
+/*
 Team Members: 
 Yuancen Pu
 */
 #ifndef HW2_HEADER
 #define HW2_HEADER
-#include <stdlib.h>
 #include <stdio.h>
-int polynomial(char* []);
+#include<stdlib.h>
+int polynomial(int);
+int argvToInt(int, char*);
 #endif
